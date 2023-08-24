5a4ae833de18f05b225d4fbaf7bb07070b0c136c
Vipinii <133142965+Vipinii@users.noreply.github.com>
2023-08-25 02:50:56 +0530

    Task-3.1P.ino

--- Task-3.1P.ino
+++ Task-3.1P.ino
@@ -3,23 +3,18 @@
 #include <ThingSpeak.h>
 #include <DHT.h>
 
-// Replace with your network credentials
 char ssid[] = "Galaxy";
 char password[] = "1234567811";
 
-// Replace with your ThingSpeak API key
 const char* apiKey = "KWPVGTO697B58EWU";
 
-// DHT sensor setup
-#define DHTPIN 2       // Pin where the DHT22 is connected
-#define DHTTYPE DHT22  // DHT 22 (AM2302)
+#define DHTPIN 2
+#define DHTTYPE DHT22
 
 DHT dht(DHTPIN, DHTTYPE);
 
-// ThingSpeak channel setup
 WiFiClient client;
 
-// Replace with your ThingSpeak channel ID
 unsigned long channelID = 2250586;
 
 void setup() {
@@ -31,7 +26,6 @@ void setup() {
     while (true);
   }
 
-  // Attempt to connect to WiFi network
   while (WiFi.begin(ssid, password) != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi...");
@@ -42,11 +36,9 @@ void setup() {
 }
 
 void loop() {
-  // Read DHT sensor data
   float humidity = dht.readHumidity();
   float temperature = dht.readTemperature();
 
-  // Check if any reads failed and exit if so
   if (isnan(humidity) || isnan(temperature)) {
     Serial.println("Failed to read from DHT sensor!");
     delay(2000);
@@ -56,7 +48,6 @@ void loop() {
   Serial.print(temperature);
   Serial.println(" °C");
 
-  // Send data to ThingSpeak
   ThingSpeak.setField(1, temperature);
   ThingSpeak.setField(2, humidity);
   int response = ThingSpeak.writeFields(channelID, apiKey);
@@ -68,5 +59,5 @@ void loop() {
     Serial.println(response);
   }
   
-  delay(20000); // Delay for 20 seconds before sending the next data
+  delay(20000);
 }
